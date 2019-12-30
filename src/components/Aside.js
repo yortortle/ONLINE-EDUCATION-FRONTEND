c3b8022d9d7afe613f042bf7ccda23f8fb1990a9
Troy Hancock <Troy@Troys-MacBook-Pro.local>
2019-12-30 11:27:24 -0500

    pushing css changes starter app

---
+++ src/components/Aside.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class Aside extends React.Component {
+  render() {
+    return (
+      <aside className="aside-container">
+        <img src="https://p.kindpng.com/picc/s/136-1367954_healthcare-and-education-icon-png-download-healthcare-and.png" className="icon"/>
+          <ul className="aside-list">
+            <div className="sections">
+              <li className="sections"> Nav </li>
+              <div className="dropdown-divider" ></div>
+            </div>
+            <li className="dropdown-item"> Home </li>
+            <li className="dropdown-item"> Mission </li>
+            <li className="dropdown-item"> About </li><br/>
+            <div className="sections">
+              <li className="sections"> Subjects </li>
+              <div className="dropdown-divider" ></div>
+            </div>
+            <li className="dropdown-item"> Art </li>
+            <li className="dropdown-item"> History </li>
+            <li className="dropdown-item"> Language </li>
+            <li className="dropdown-item"> Philosophy </li>
+            <li className="dropdown-item"> Psychology </li>
+            <li className="dropdown-item"> Mathematics </li>
+            <li className="dropdown-item"> Music Theory </li>
+            <li className="dropdown-item"> Science</li>
+          </ul>
+      </aside>
+    )
+  }
+}
+
+
+export default Aside;
---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render () {
+    return(
+      <div className="main-container">
+        <div className="test">
+          <div className="display">
+            <h1 className="display-6">Online Education</h1>
+            <p className="lead">An encyclopedia of online educational resources</p>
+          </div>
+        </div>
+        <p className=""> The Turn of the Screw is a British television film based on Henry James's 1898 ghost story of the same name. Commissioned and produced by the BBC, it was first broadcast on 30 December 2009, on BBC One. The novella was adapted for the screen by Sandy Welch, and the film was directed by Tim Fywell. Although generally true to the tone and story of James's work, the film is set in the 1920s instead of the 1840s. The story is told in flashbacks during consultations between the institutionalised Ann, played by Michelle Dockery (pictured), and a psychiatrist, Dr Fisher (Dan Stevens). Ann tells how she was hired by an aristocrat (Mark Umbers) to care for the orphans Miles (Josef Lindsay) and Flora (Eva Sayer) at their home, Bly House. Ann soon begins to see unknown figures around the manor, and seeks an explanation. Though the film generally received a positive response, critics disagreed over whether it retained the novella's much-discussed ambiguity. (Full article...)</p>
+      </div>
+    )
+  }
+}
+export default Main;
